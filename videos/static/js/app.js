69f6f52cfec5312a02f674671b844d280b095e3c
Stephen Delaney <sdelaney@skokielibrary.info>
2016-08-29 16:43:46 -0500

    removed bootstrap grid, added css and scripts to make sidebar responsive and animated

--- videos/static/js/app.js
+++ videos/static/js/app.js
@@ -17,4 +17,53 @@ $( document ).ready(function() {
       $(".moreinfo-sidebar").html("Hover over any video for more information!")
       $("h1").css("text-decoration", "none")
     })
+
+    // Testing
+    var windw = this;
+
+    $.fn.followTo = function ( pos ) {
+        var $this = this,
+            $window = $(windw);
+
+        $window.scroll(function(e){
+            if ($window.scrollTop() > pos) {
+              if ($this.css("top") == "260px") {
+                $this.removeClass("fixed_sidebar");
+                $this.animate({
+                  top: "50px"
+                })
+              }
+
+            } else {
+              if ($this.css("top") == "50px") {
+                $this.animate({
+                  top: "260px"
+                })
+              }
+
+
+            }
+        });
+    };
+
+    $('.moreinfo-sidebar').followTo(190);
+
+    function change_class_on_resize(){
+      var window_width = $( window ).width();
+      // console.log(window_width)
+      if (window_width < 1350) {
+        $(".individual_video").addClass("clear_class")
+      } else {
+        $(".individual_video").removeClass("clear_class")
+      }
+    }
+
+    change_class_on_resize();
+
+    $( window ).resize(function() {
+      change_class_on_resize();
+    });
+
+
+
 });
